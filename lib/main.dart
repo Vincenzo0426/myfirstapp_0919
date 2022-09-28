6561320676883a49ed405448be526cea4141d406
Vincenzo0426 <113822102+Vincenzo0426@users.noreply.github.com>
2022-09-28 22:52:58 +0800

    first commit_0929

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   // 建立appTitle和hiFlutter物件
   var appTitle = Text('我的第一個Flutter App'),
-      hiFlutter = Text('Hi, Flutter.',
-        style: TextStyle(fontSize: 50),
+      hiFlutter = Text(' Hi, Flutter. \n 今天是20220928',
+        style: TextStyle(fontSize: 40),
       );
 
   // 建立appBody物件
